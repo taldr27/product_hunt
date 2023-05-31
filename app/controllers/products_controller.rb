3fc6a26f999db44a32bb679abe08f56325351a62
lana <diegogarciaq98@gmail.com>
2023-05-31 01:09:06 -0500

    Add errors to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,11 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(product_params)
-    redirect_to products_path, notice:"Product have been saved."
+    if @product.persisted?
+      redirect_to products_path, notice:"Product have been saved."
+    else
+      render :new, status: :unprocessable_entity 
+    end
   end
 
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: {message: 'Product Name is required'}
+  validates :description, presence: {message: 'Product Description is required'}
+
+  validates :name, length: {maximum: 200, minimum: 2}
 end
